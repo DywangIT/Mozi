04fadaeac73e7bf4222ef57fd2ec8fa353efdaab
Quang <dyquang5083@gmail.com>
2025-03-22 14:33:56 +0700

    fixed

--- mozi.js
+++ mozi.js
@@ -9,14 +9,18 @@ function Mozi(options = {}) {
         },
         options
     );
-    this.template = document.querySelector(`#${this.opt.templateId}`);
-    if (!this.template) {
+    this._template = document.querySelector(`#${this.opt.templateId}`);
+    if (!this._template) {
         console.error(`#${this.opt.templateId} does not exists!`);
         return;
     }
-    this.closeByX = this.opt.closeMethods.includes("x");
-    this.closeByOverlay = this.opt.closeMethods.includes("overlay");
-    this.closeByEscape = this.opt.closeMethods.includes("escape");
+    this._closeByX = this.opt.closeMethods.includes("x");
+    this._closeByOverlay = this.opt.closeMethods.includes("overlay");
+    this._closeByEscape = this.opt.closeMethods.includes("escape");
+    
+    this._modalFooter = document.createElement("div");
+    this._modalFooter.classList.add("mozi__footer");
+    this._footerButtons = [];
 }
 
 Mozi.prototype._createButton = function (title, cssClass, callback) {
@@ -28,8 +32,8 @@ Mozi.prototype._createButton = function (title, cssClass, callback) {
 };
 
 Mozi.prototype._renderFooterButton = function () {
-    this.footerButtons.forEach((button) => {
-        this.modalFooter.append(button);
+    this._footerButtons.forEach((button) => {
+        this._modalFooter.append(button);
     });
 };
 
@@ -38,45 +42,42 @@ Mozi.prototype._handleEscapeKey = function (e) {
     if (e.key === "Escape" && lastModal === this) this.close();
 };
 
-Mozi.prototype.build = function () {
-    this.backdrop = document.createElement("div");
-    this.backdrop.classList.add("mozi__backdrop");
+Mozi.prototype._build = function () {
+    this._backdrop = document.createElement("div");
+    this._backdrop.classList.add("mozi__backdrop");
     const container = document.createElement("div");
     container.classList.add("mozi__container");
     const modalContent = document.createElement("div");
     modalContent.classList.add("mozi__content");
-    modalContent.append(this.template.content.cloneNode(true));
+    modalContent.append(this._template.content.cloneNode(true));
     container.append(modalContent);
-    this.backdrop.append(container);
-    document.body.append(this.backdrop);
+    this._backdrop.append(container);
+    document.body.append(this._backdrop);
 
-    if (this.closeByX) {
+    if (this._closeByX) {
         const xClose = this._createButton("&times;", "mozi__close", this.close);
         container.insertBefore(xClose, modalContent);
     }
 
-    if (this.closeByOverlay) {
-        this.backdrop.onclick = (e) => {
-            if (e.target === this.backdrop) this.close();
+    if (this._closeByOverlay) {
+        this._backdrop.onclick = (e) => {
+            if (e.target === this._backdrop) this.close();
         };
     }
 
-    if (this.closeByEscape) {
+    if (this._closeByEscape) {
         this._handleEscapeKey = this._handleEscapeKey.bind(this);
         document.addEventListener("keydown", this._handleEscapeKey);
     }
 
     if (this.opt.footer) {
-        this.modalFooter = document.createElement("div");
-        this.modalFooter.classList.add("mozi__footer");
-        this.footerButtons = [];
         this._renderFooterButton();
-        container.append(this.modalFooter);
+        container.append(this._modalFooter);
     }
 };
 
 Mozi.prototype._getScrollbarWidth = function () {
-    if (this.scrollbarWidth) return this.scrollbarWidth;
+    if (this._scrollbarWidth) return this._scrollbarWidth;
 
     const div = document.createElement("div");
     document.body.appendChild(div);
@@ -86,22 +87,22 @@ Mozi.prototype._getScrollbarWidth = function () {
         top: "-9999px"
     });
 
-    this.scrollbarWidth = div.offsetWidth - div.clientWidth;
+    this._scrollbarWidth = div.offsetWidth - div.clientWidth;
     document.body.removeChild(div);
-    return this.scrollbarWidth;
+    return this._scrollbarWidth;
 };
 
 Mozi.prototype.open = function () {
     Mozi.elements.push(this);
-    if (!this.backdrop) this.build();
-    setTimeout(() => this.backdrop.classList.add("mozi--show"), 100);
+    if (!this._backdrop) this._build();
+    setTimeout(() => this._backdrop.classList.add("mozi--show"), 100);
     document.body.classList.add("mozi--no-scroll");
     document.body.style.paddingRight = this._getScrollbarWidth() + "px";
 };
 
 Mozi.prototype.close = function () {
     Mozi.elements.pop();
-    this.backdrop.classList.remove("mozi--show");
+    this._backdrop.classList.remove("mozi--show");
     if (!Mozi.elements.length) {
         document.body.classList.remove("mozi--no-scroll");
         document.body.style.paddingRight = "";
@@ -109,12 +110,12 @@ Mozi.prototype.close = function () {
 };
 
 Mozi.prototype.setFooter = function (html) {
-    this.modalFooter.innerHTML = html;
+    this._modalFooter.innerHTML = html;
     this._renderFooterButton();
 };
 
-Mozi.prototype._addFooterButton = function (title, cssClass, callback) {
+Mozi.prototype.addFooterButton = function (title, cssClass, callback) {
     const button = this._createButton(title, cssClass, callback);
-    this.footerButtons.push(button);
+    this._footerButtons.push(button);
     this._renderFooterButton();
 };
